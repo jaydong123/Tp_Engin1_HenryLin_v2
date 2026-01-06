971d1f849e80f9d8d3c63540a08506cfff817bc7
jaydong123 <henry_e_heng_lin@outlook.com>
2026-01-06 15:54:52 -0500

    Finish rotate
    It finally works after x numbers of days

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -6,6 +6,7 @@ public class PlayerInputHandler : MonoBehaviour
 {
     public UnityEvent<Vector2> OnMoveInput;
     public UnityEvent OnJumpInput;
+    public UnityEvent <Vector2>OnMouseMoveInput;
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -16,5 +17,12 @@ public class PlayerInputHandler : MonoBehaviour
     {
         if (context.performed || context.canceled)
             OnJumpInput?.Invoke();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnLook called, context.phase: " + context.phase);
+        if (context.performed || context.canceled)
+            OnMouseMoveInput?.Invoke(context.ReadValue<Vector2>());
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Camera cam;
+    private Vector2 mousePosition;
     private Rigidbody rb;
     private PlayerInputHandler playerInputHandler;
     [SerializeField] private float speed = 5;
-    [SerializeField] private float jumpForce = 100;
+    [SerializeField] private float jumpForce = 500;
     [SerializeField] private Vector3 moveDirection;
+    [SerializeField] private Vector3 rotateDirection;
+    [SerializeField] private float rotateSpeed = 5.0f;
+    
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -17,11 +22,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(moveDirection * speed);
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        transform.position += (moveDirection * (speed * Time.deltaTime));
     }
 
     public void Move(Vector2 input)
     {
+        Debug.Log("Im at Move");
         input.Normalize();
         moveDirection = new Vector3(input.x, 0, input.y);
     }
@@ -30,5 +41,17 @@ public class PlayerMovement : MonoBehaviour
     public void Jump()
     {
         rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Force);
+    }
+
+    public void Rotate(Vector2 input)
+    {
+        Debug.Log("Im at Rotate");
+        mousePosition = input;
+        Vector3 worldPosition =  cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, cam.nearClipPlane));
+        rotateDirection = (worldPosition - transform.position).normalized;
+        rotateDirection.z = 0;
+        
+        float angle = Mathf.Atan2(rotateDirection.y, rotateDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
